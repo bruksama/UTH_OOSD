245cb9cdd37e17228e209e4e50986cec1b61664d
ynnn7451-prog <ynnn7451@gmail.com>
2026-01-16 23:16:01 +0700

    Update dashboard UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,24 +7,24 @@ import Alerts from './pages/Alerts';
 
 /**
  * Main Application Component
- * 
+ *
  * Provides routing and layout structure for SPTS frontend.
- * 
+ *
  * @author SPTS Team
  */
 function App() {
-  return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/students" element={<Students />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/alerts" element={<Alerts />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/students" element={<Students />} />
+                    <Route path="/courses" element={<Courses />} />
+                    <Route path="/alerts" element={<Alerts />} />
+                </Routes>
+            </Layout>
+        </Router>
+    );
 }
 
 export default App;
